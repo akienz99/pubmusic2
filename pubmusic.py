b3d5a93578aeba9a3aa2a27c09cfadbc21f23c15
L1n3m4st3r <andre.kienzle.ak@gmail.com>
2016-12-18 11:58:13 +0100

    added logging features to main procedure and playerCtl class

--- pubmusic.py
+++ pubmusic.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 from vlcclient import VLCClient
+from ezlogger import ezLogger
 
 print ("#######################################################")
 print ("#  _____       _                         _      ___   #")
@@ -15,10 +16,17 @@ print ("#######################################################")
 
 project_version = "0.0.1-alpha1"
 
+# Initializing our logger for use in playerCtl
+logger = ezLogger(1)
+
+logger.dispLogEntry("info", "initializing, please wait...")
+
 os.system("nohup /usr/bin/vlc --intf telnet  --telnet-password admin &>/dev/null &")
 #os.spawnl(os.P_NOWAIT, '/usr/bin/vlc --intf telnet  --telnet-password admin')
 time.sleep(1)
-  
+
+logger.dispLogEntry("welcome", "Welcome to Pubmusic2, version " + project_version)
+
 class playerCtl:
 
    # TODO: Playlist syncronisation with VLC
@@ -31,32 +39,41 @@ class playerCtl:
    vlc = VLCClient("::1")
    
    currentPlaying = None
+   currentPlayingFromVlc = ""
+   playerStatus = "running"
    nextPlaying = []
+   # FIXME: Proper Thread termination   
+   threadStopper = False
 
    def __init__(self):
       self.vlc.connect()
-            
-     
+      logger.dispLogEntry("info", "connected to vlc media player")
+         
    def add(self, filepath):
       self.vlc.add(filepath)     
       self.currentPlaying = filepath
-       
+      logger.dispLogEntry("playlist", "Now playing: " + self.getCleanTitle(filepath))
+      
    def enqueue(self, filepath):
       self.vlc.enqueue(filepath)
       self.nextPlaying.append(filepath)
+      logger.dispLogEntry("playlist", "Added to queue: " + self.getCleanTitle(filepath))
       
    def next(self):
       self.vlc.next()
       
    def volume(self, vol):
       self.vlc.volume(vol)
-       
+      logger.dispLogEntry("info","setting volume to " + str(vol))
+      
    def volup(self):
       self.vlc.volup()
+      logger.dispLogEntry("info","raising volume")
       
    def voldown(self):
       self.vlc.voldown()
-       
+      logger.dispLogEntry("info","lowering volume")
+      
    def stop(self):
       self.vlc.stop()
       
@@ -64,10 +81,12 @@ class playerCtl:
       self.vlc.clear()
       self.nextPlaying = []
       self.currentPlaying = None
-       
+      logger.dispLogEntry("warning","cleared playlist")
+      
    def shutdown(self):
       self.vlc.raw("shutdown")
       self.stopTitleChangeNotifierThread()
+      logger.dispLogEntry("info","Shutting down vlc client")
       
    def getVlcInternalCurrentTitle(self):
       return self.vlc.raw("get_title")
