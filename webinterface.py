ffd6ebd942d5450ab7bba914c93d85254755baf8
André Kienzle <andre.kienzle.ak@gmail.com>
2017-01-02 13:39:47 +0100

    first running version of the webserver, added cherrypy as dependency

--- webinterface.py
+++ webinterface.py
@@ -1,11 +1,47 @@
 import cherrypy
+from threading import Thread
 
 class webInterface:
    
-   def __init__(self):
-      cherrypy.quickstart(webContent())
-
-class webContent(object):
-   def index(self):
-      return "Hello World!"
+   webThread = None
    
+   # Variables for used objects from pubmusic.py
+   # See this file for init
+   logger = None
+   player = None
+   library = None
+   
+   def __init__(self, logger, player, library):
+      # Setting the class variables to our objects
+      self.logger = logger
+      self.player = player
+      self.library = library
+      
+      self.webThread = Thread(target=self.webInterfaceThread).start()
+
+      
+   def webInterfaceThread(self):
+      cherrypy.config.update({'log.screen': False})
+      cherrypy.quickstart(webApplication(self.logger, self.player, self.library), '/')
+      
+   def getPlayerObject(self):
+      return self.player
+
+class webApplication(object):
+   # Variables for used objects from pubmusic.py
+   # See this file for init
+   logger = None
+   player = None
+   library = None
+   
+   
+   def __init__(self, logger, player, library):
+      # Setting the class variables to our objects
+      self.logger = logger
+      self.player = player
+      self.library = library
+   
+   @cherrypy.expose
+   def index(self):
+      return self.player.getCurrentPlaying()
+        
